ca405bfcf64a26c4dd2ba3439b4ca4aaf5aa6969
VladTGeorgiev <vlad.georgiev@outlook.com>
2019-10-29 19:39:22 +0000

    User and Task models, validation added

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,23 +1,75 @@
 const mongoose = require('mongoose');
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
-    useUnifiedTopology: true,
+	useUnifiedTopology: true,
 	useNewUrlParser: true,
 	useCreateIndex: true
 });
 
-const User = mongoose.model('User', {
-	name: {
-		type: String
+// const User = mongoose.model('User', {
+// 	name: {
+//         type: String,
+//         required: true,
+//         trim: true
+//     },
+//     password: {
+//         type: String,
+//         required: true,
+//         trim: true,
+//         minlength: 7,
+//         validate(value){
+//             if(value.toLowerCase().includes('password')) {
+//                 throw new Error('Password cannot contain "password"')
+//             }
+//         }
+
+//     },
+//     email: {
+//         type: String,
+//         required: true,
+//         lowercase: true,
+//         trim: true,
+//         validate(value) {
+//             if(!validator.isEmail(value)){
+//                 throw new Error('E-mail is invalid')
+//             }
+//         }
+//     },
+// 	age: {
+//         type: Number,
+//         default: 0,
+//         validate(value) {
+//             if(value<0){
+//                 throw new Error('Age must be a positive number')
+//             }
+//         }
+// 	}
+// });
+
+// const userOne = new User({
+//     name: '      Vlad',
+//     email: 'vlad@gmail.com            ',
+//     password: 'phone123!         '
+// });
+
+// userOne.save().then((userOne) => console.log(userOne)).catch((error) => console.log('Error', error));
+
+const Task = mongoose.model('Task', {
+	description: {
+        type: String,
+        required: true,
+        trim: true,
 	},
-	age: {
-		type: Number
+	completed: {
+        type: Boolean,
+        default: false
 	}
 });
 
-const userOne = new User({
-	name: 'Vlad',
-	age: "kguk"
-});
+const taskOne = new Task({
+    description: 'Get a job!',
+    completed: false
+})
 
-userOne.save().then((userOne) => console.log(userOne)).catch((error) => console.log('Error', error));
+taskOne.save().then(res => console.log(res)).catch(error => console.log(error))
