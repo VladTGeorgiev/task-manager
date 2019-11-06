2387dd66d4c7cb16d83a74843e5ed67c1922da84
VladTGeorgiev <vlad.georgiev@outlook.com>
2019-11-06 00:18:51 +0000

    Task update route added

--- src/index.js
+++ src/index.js
@@ -45,26 +45,27 @@ app.get('/users/:id', async (req, res) => {
 });
 
 app.patch('/users/:id', async (req, res) => {
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['name', 'email', 'password', 'age']
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+	const _id = req.params.id;
+	const updates = Object.keys(req.body);
+	const allowedUpdates = [ 'name', 'email', 'password', 'age' ];
+	const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
 
-    if (!isValidOperation) {
-        return res.status(400).send({ error: 'Invalid updates!' })
-    }
+	if (!isValidOperation) {
+		return res.status(400).send({ error: 'Invalid updates!' });
+	}
 
-    try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
-    
-        if (!user) {
-            return res.status(404).send()
-        }
+	try {
+		const user = await User.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true });
 
-        res.send(user)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
+		if (!user) {
+			return res.status(404).send();
+		}
+
+		res.send(user);
+	} catch (error) {
+		res.status(400).send(error);
+	}
+});
 
 // TASK
 
@@ -99,6 +100,27 @@ app.get('/tasks/:id', async (req, res) => {
 		res.status(200).send(task);
 	} catch (error) {
 		res.status(500).send(error);
+	}
+});
+
+app.patch('/tasks/:id', async (req, res) => {
+	const _id = req.params.id;
+	const updates = Object.keys(req.body);
+	const allowedUpdates = [ 'description', 'completed' ];
+	const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+
+	if (!isValidOperation) {
+		return res.status(400).send({ error: 'Invalid updates!' });
+	}
+
+	try {
+		const task = await Task.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true });
+		if (!task) {
+			return res.status(404).send();
+		}
+		res.status(200).send(task);
+	} catch (error) {
+		res.status(400).send(error);
 	}
 });
 
