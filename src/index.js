f393146414e4e6ab6f91b2e2ff1c4936b5d0940a
VladTGeorgiev <vlad.georgiev@outlook.com>
2019-11-06 00:31:43 +0000

    Delete routes for users and tasks added

--- src/index.js
+++ src/index.js
@@ -67,6 +67,18 @@ app.patch('/users/:id', async (req, res) => {
 	}
 });
 
+app.delete('/users/:id', async (req, res) => {
+	try {
+		const user = await User.findByIdAndDelete(req.params.id);
+		if (!user) {
+			return res.status(404).send();
+		}
+		res.status(200).send(user);
+	} catch (error) {
+		res.status(500).send();
+	}
+});
+
 // TASK
 
 app.post('/tasks', async (req, res) => {
@@ -121,6 +133,18 @@ app.patch('/tasks/:id', async (req, res) => {
 		res.status(200).send(task);
 	} catch (error) {
 		res.status(400).send(error);
+	}
+});
+
+app.delete('/tasks/:id', async (req, res) => {
+	try {
+		const task = await Task.findByIdAndDelete(req.params.id);
+		if (!task) {
+			return res.status(404).send();
+		}
+		res.status(200).send(task);
+	} catch (error) {
+		res.status(500).send();
 	}
 });
 
